ecdaf4836cedada0a4bddcf03d865f1a864763f1
drsky <contact@drsky.ovh>
2023-03-09 15:05:26 +0100

    ajout de l'animation du coeur

--- scripts/template/MediaCard.js
+++ scripts/template/MediaCard.js
@@ -72,6 +72,17 @@ export class MediaCard {
   }
 
   /**
+   * heart animation management
+   */
+  animeHeart() {
+    const maskElement = this._wrapper.querySelector('.heart__mask');
+    if (maskElement.classList.contains('heart--liked')) {
+      maskElement.classList.remove('heart--liked');
+    }
+    maskElement.classList.toggle('heart-anime');
+  }
+
+  /**
    * Handling of the "like" on photos.
    */
   handleLikeButton() {
@@ -87,16 +98,16 @@ export class MediaCard {
         if (this._liked) {
           nbr.innerText = parseInt(nbr.innerText, 10) - 1;
           this._Observer.fire('DECR');
-          this._liked = false;
           this.updateLocalStorage(false);
           this._media.decrementLike();
         } else {
           nbr.innerText = parseInt(nbr.innerText, 10) + 1;
           this._Observer.fire('INCR');
-          this._liked = true;
           this.updateLocalStorage(true);
           this._media.incrementLike();
         }
+        this._liked = !this._liked;
+        this.animeHeart();
       });
   }
 
@@ -125,7 +136,26 @@ export class MediaCard {
           <a href="" class="gallery-card__like" aria-label="likes" tabindex="0">
             <span class="gallery-card__nbrLike">
             </span>
-            <i class="fa-solid fa-heart"></i>
+            <div class="heart">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="-19 -19 550 550"
+            >
+              <path
+                class="heart__fill"
+                d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"
+              />
+              <path
+                class="heart__mask 
+                ${this._liked ? 'heart--liked heart-anime' : ''}"
+                d="M0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96z"
+              />
+              <path
+                class="heart__stroke"
+                d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z"
+              />
+            </svg>
+            </div>
           </a>
         </figcaption>
       </figure>
